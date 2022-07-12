fd0519145956003faa2c842250d3fc9add9f6790
SirriRyisa <sirriryisa@gmail.com>
2022-07-12 09:27:26 +0100

    Able to show users and controllers works just fine

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # get 'home/index'
-  get 'posts/posts'
-  get 'home/home'
-  get 'users/users'
+  # get 'posts/posts'
+  # get 'home/home'
+  # get 'users/users'
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_31_120248) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_01_165722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
